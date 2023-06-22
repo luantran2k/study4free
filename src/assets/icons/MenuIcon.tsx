6fba64634b44ed01f3fa46f0ff6ebf3f6cd4ec01
huannguyen24 <huannguyen2410@gmail.com>
2023-06-22 21:27:58 +0700

    coi nhu la xong cai hinh dictionary, de Long xu ly function

---
+++ src/assets/icons/MenuIcon.tsx
@@ -0,0 +1,21 @@
+import { IIcon } from '../../interfaces';
+
+export const MenuIcon = ({ width, height, fill }: IIcon) => {
+    return (
+        <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            viewBox="0 0 20 20"
+            fill={fill}
+            width={width}
+            height={height}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+            ></path>
+        </svg>
+    );
+};
--- src/pages/layouts/Root/index.tsx
+++ src/pages/layouts/Root/index.tsx
@@ -1,36 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from '../../../components/common/Header';
 import { Footer } from '../../../components/common/Footer';
-import React, { useEffect, useRef, useState } from 'react';
-import Tools from '../../../components/common/Tools';
-import { useInView } from 'react-intersection-observer';
 const RootLayout = () => {
-  const [isIntersecting, setIsIntersecting] = useState(false);
-
-  const ref = useRef(null);
-
-  useEffect(() => {
-    window.addEventListener('scroll', (e) => {
-      // console.log(e.target.);
-    });
-    // const myObserver = new IntersectionObserver(
-    //   ([entry]) => {
-    //     setIsIntersecting(entry.isIntersecting);
-    //   },
-    //   { rootMargin: '-10px' }
-    // );
-    // if (ref.current) {
-    //   myObserver.observe(ref.current);
-    // }
-  }, []);
-
   return (
     <div className="h-full relative">
       <Header />
-      <div ref={ref}>asdasdsd</div>
       <Outlet />
       <Footer />
-      <Tools />
     </div>
   );
 };
