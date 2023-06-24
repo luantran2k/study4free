f7a2f1fc78c18fa8d9fa37e87f05f86b85560b69
DuongVanMinh <duongvanminh241220@gmail.com>
2023-06-24 17:15:36 +0700

    update logout3

--- src/pages/UserProfile/index.tsx
+++ src/pages/UserProfile/index.tsx
@@ -101,7 +101,6 @@ function UserProfilePage() {
           </NavLink>
           <NavLink
             to="*"
-            onClick={onLogOut}
             className="text-[24px] w-[100%] flex justify-center py-5"
             style={({ isActive }) => {
               return {
@@ -113,7 +112,8 @@ function UserProfilePage() {
             <DeletedUserIcon />
           </NavLink>
           <NavLink
-            to="/login"
+            to="/"
+            onClick={onLogOut}
             className="text-[24px] w-[100%] flex justify-center py-5"
             style={({ isActive }) => {
               return {
