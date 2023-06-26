da9698312114bd437d9ad16592d4e919ab52ca01
LongTuan-123 <85407964+LongTuan-123@users.noreply.github.com>
2023-06-26 11:32:20 +0700

    add notvip page

--- src/pages/UserProfile/AnalyzeResults.tsx
+++ src/pages/UserProfile/AnalyzeResults.tsx
@@ -12,6 +12,10 @@ import {
 
 import { Line } from 'react-chartjs-2';
 import { Pie } from 'react-chartjs-2';
+import { useSelector } from 'react-redux';
+import { useGetUserByIdQuery } from '../../store/queries/users';
+import { RootState } from '../../store';
+import NotVipPlayer from '../NotFound/NotVipPlayer';
 
 ChartJS.register(
   CategoryScale,
@@ -32,7 +36,7 @@ const options = {
     },
     title: {
       display: true,
-      text: 'Listening results'
+      text: 'Listening results',
     },
   },
 };
@@ -79,190 +83,198 @@ const dataForPie = {
 };
 
 function AnalyzeResults() {
-    return (
-      <div>
-        <h3 className="text-center text-[40px] font-medium my-[20px]">
-          Analyze Results
-        </h3>
-        <div className="flex justify-around text-[30px] flex-wrap gap-5">
-          <span
-            className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
+  const user = useSelector((state: RootState) => state.auth.userInformation);
+  const { data, isLoading, isSuccess } = useGetUserByIdQuery(user?.id);
+  return (
+    <div>
+      {!data?.payment ? (
+        <NotVipPlayer />
+      ) : (
+        <>
+          <h3 className="text-center text-[40px] font-medium my-[20px]">
+            Analyze Results
+          </h3>
+          <div className="flex justify-around text-[30px] flex-wrap gap-5">
+            <span
+              className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
           cursor-pointer min-w-[200px] hover:bg-[#40634d] hover:text-[#fff]"
-          >
-            Listening
-          </span>
-          <span
-            className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
-          cursor-pointer min-w-[200px] hover:bg-[#40634d] hover:text-[#fff]"
-          >
-            Reading
-          </span>
-          <span
-            className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
-          cursor-pointer min-w-[200px] hover:bg-[#40634d] hover:text-[#fff]"
-          >
-            Speaking
-          </span>
-          <span
-            className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
-          cursor-pointer min-w-[200px] hover:bg-[#40634d] hover:text-[#fff]"
-          >
-            Writing
-          </span>
-        </div>
-        <div className="grid grid-rows-1 grid-cols-12 px-[40px] pt-[50px] gap-[20px]">
-          <div className="col-span-9 max-lg:col-span-12 max-lg:flex max-lg:flex-col max-lg:items-center">
-            <div className="mb-[30px] max-lg:hidden">
-              <Line options={options} data={dataForLine} />
-            </div>
-            <div className="mb-[30px] hidden max-lg:block">
-              <Pie data={dataForPie} />
-            </div>
-            <div className="flex justify-around gap-[10px] mb-6 flex-wrap">
-              <span
-                className="bg-[#fff]  text-center px-[10px] py-[20px] rounded-xl 
-              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
-              >
-                <p className="text-[20px]">Number exams</p>
-                <p className="font-bold text-[30px]">15</p>
-              </span>
-              <span
-                className="bg-[#fff] text-center px-[10px] py-[20px] rounded-xl 
-              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
-              >
-                <p className="text-[20px]">Accuracy</p>
-                <p className="font-bold text-[30px]">64.50%</p>
-              </span>
-              <span
-                className="bg-[#fff] text-center px-[10px] py-[20px] rounded-xl 
-              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
-              >
-                <p className="text-[20px]">Average time</p>
-                <p className="font-bold text-[30px]">02:20:34</p>
-              </span>
-              <span
-                className="bg-[#fff] text-center px-[10px] py-[20px] rounded-xl 
-              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
-              >
-                <p className="text-[20px]">Average score</p>
-                <p className="font-bold text-[30px]">6.5</p>
-              </span>
-            </div>
-          </div>
-          <div className='mt-12 col-span-3 max-h-[400px] overflow-scroll max-lg:hidden'>
-            <table
-              className="shadow-lg table-auto border-collapse
-            border border-slate-500 w-[100%]"
             >
-              <thead>
-                <tr>
-                  <th className="border border-slate-600">Exams</th>
-                  <th className="border border-slate-600">Results</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 4
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    19/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 2
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    28/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 1
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    18/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 4
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    19/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 2
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    28/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 1
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    18/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 1
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    18/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 1
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    18/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 2
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    28/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 1
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    18/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 1
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    18/40
-                  </td>
-                </tr>
-                <tr>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    C17 IELTS listening test 1
-                  </td>
-                  <td className="border border-slate-700 text-center py-[10px]">
-                    18/40
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+              Listening
+            </span>
+            <span
+              className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
+          cursor-pointer min-w-[200px] hover:bg-[#40634d] hover:text-[#fff]"
+            >
+              Reading
+            </span>
+            <span
+              className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
+          cursor-pointer min-w-[200px] hover:bg-[#40634d] hover:text-[#fff]"
+            >
+              Speaking
+            </span>
+            <span
+              className="bg-[#fff] shadow-xl py-[20px] px-[40px] rounded-3xl 
+          cursor-pointer min-w-[200px] hover:bg-[#40634d] hover:text-[#fff]"
+            >
+              Writing
+            </span>
           </div>
-        </div>
-      </div>
-    );
+          <div className="grid grid-rows-1 grid-cols-12 px-[40px] pt-[50px] gap-[20px]">
+            <div className="col-span-9 max-lg:col-span-12 max-lg:flex max-lg:flex-col max-lg:items-center">
+              <div className="mb-[30px] max-lg:hidden">
+                <Line options={options} data={dataForLine} />
+              </div>
+              <div className="mb-[30px] hidden max-lg:block">
+                <Pie data={dataForPie} />
+              </div>
+              <div className="flex justify-around gap-[10px] mb-6 flex-wrap">
+                <span
+                  className="bg-[#fff]  text-center px-[10px] py-[20px] rounded-xl 
+              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
+                >
+                  <p className="text-[20px]">Number exams</p>
+                  <p className="font-bold text-[30px]">15</p>
+                </span>
+                <span
+                  className="bg-[#fff] text-center px-[10px] py-[20px] rounded-xl 
+              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
+                >
+                  <p className="text-[20px]">Accuracy</p>
+                  <p className="font-bold text-[30px]">64.50%</p>
+                </span>
+                <span
+                  className="bg-[#fff] text-center px-[10px] py-[20px] rounded-xl 
+              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
+                >
+                  <p className="text-[20px]">Average time</p>
+                  <p className="font-bold text-[30px]">02:20:34</p>
+                </span>
+                <span
+                  className="bg-[#fff] text-center px-[10px] py-[20px] rounded-xl 
+              shadow-md hover:bg-[#40634d] hover:text-[#fff] cursor-pointer min-w-[180px]"
+                >
+                  <p className="text-[20px]">Average score</p>
+                  <p className="font-bold text-[30px]">6.5</p>
+                </span>
+              </div>
+            </div>
+            <div className="mt-12 col-span-3 max-h-[400px] overflow-scroll max-lg:hidden">
+              <table
+                className="shadow-lg table-auto border-collapse
+            border border-slate-500 w-[100%]"
+              >
+                <thead>
+                  <tr>
+                    <th className="border border-slate-600">Exams</th>
+                    <th className="border border-slate-600">Results</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 4
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      19/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 2
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      28/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 1
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      18/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 4
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      19/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 2
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      28/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 1
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      18/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 1
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      18/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 1
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      18/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 2
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      28/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 1
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      18/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 1
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      18/40
+                    </td>
+                  </tr>
+                  <tr>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      C17 IELTS listening test 1
+                    </td>
+                    <td className="border border-slate-700 text-center py-[10px]">
+                      18/40
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default AnalyzeResults;
