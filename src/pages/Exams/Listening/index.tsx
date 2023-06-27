54376a1373d171795f7c2b2e97ff75e3fc15720d
LongTuan-123 <85407964+LongTuan-123@users.noreply.github.com>
2023-06-27 22:48:32 +0700

    add reading and listening folder

---
+++ src/pages/Exams/Listening/index.tsx
@@ -0,0 +1,4 @@
+const Listening = () => {
+  return <>Listening</>;
+};
+export default Listening;
--- src/routes/mainRoute.tsx
+++ src/routes/mainRoute.tsx
@@ -21,6 +21,7 @@ import {
 } from './imports';
 // import NotFoundPage from '../pages/NotFound';
 import Speaking from '../pages/Exams/Speaking';
+import Reading from '../pages/Exams/Reading';
 
 export const mainRoute: RouteObject = {
   path: '/',
@@ -35,14 +36,20 @@ export const mainRoute: RouteObject = {
       element: <ExamsPage />,
       children: [
         {
+          path: 'Writing/:skill',
+          element: <Writing />,
+        },
+        {
           path: 'Speaking/:skill',
           element: <Speaking />,
-          children: [
-            {
-              path: ':partId',
-              element: <Speaking />,
-            },
-          ],
+        },
+        {
+          path: 'Reading/:skill',
+          element: <Reading />,
+        },
+        {
+          path: 'Writing/:skill',
+          element: <Writing />,
         },
         // {
         //   path: 'Speaking/:skill',
