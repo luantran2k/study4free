f62441e673efbbf5b61e071ae0dedfba12c42d62
LongTuan-123 <85407964+LongTuan-123@users.noreply.github.com>
2023-06-28 13:51:59 +0700

    add reading logic

---
+++ src/interfaces/SectionResponse.ts
@@ -0,0 +1,12 @@
+export interface ISectionResponse {
+  id: string;
+  section: 'Listening' | 'Reading' | 'Writing' | 'Speaking';
+  questions: {
+    id: string;
+    answers: {
+      id: string;
+      value?: string;
+      audio?: File;
+    }[];
+  }[];
+}
--- src/pages/Exams/Reading/index.tsx
+++ src/pages/Exams/Reading/index.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useGetPartByIdQuery } from '../../../store/queries/exams';
 import NavigationTest from '../commonComponent/navigationTest';
+import { IReadingAnswer } from '../../../interfaces/Reading';
+import { ISectionResponse } from '../../../interfaces/SectionResponse';
+import { useLocation } from 'react-router-dom';
 
 const Reading = () => {
   const [index, setIndex] = useState<number>(0);
   const [partId, setPartId] = useState<string>('');
+  const location = useLocation();
+  const [answersArr, setAnswersArr] = useState<ISectionResponse>({
+    id: location.pathname.split('/')[3],
+    section: 'Reading',
+    questions: [{ id: partId, answers: [] }],
+  });
+
   const { data, isSuccess, error, isLoading } = useGetPartByIdQuery({
     section: 'Reading',
     partId: partId,
@@ -17,6 +27,52 @@ const Reading = () => {
   const handleIndex = (index: number) => {
     setIndex(index);
   };
+  useEffect(() => {
+    if (partId !== '') {
+      setAnswersArr((prev: ISectionResponse) => {
+        const questionExists = prev.questions.some(
+          (question) => question.id === partId
+        );
+        if (!questionExists) {
+          const updatedQuestions = [
+            ...prev.questions.filter((question) => question.id !== ''),
+            { id: partId, answers: [] },
+          ];
+          return { ...prev, questions: updatedQuestions };
+        }
+        return prev;
+      });
+    }
+  }, [partId]);
+
+  const handleGetValue = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    item: IReadingAnswer
+  ) => {
+    setAnswersArr((prev: ISectionResponse) => {
+      const updatedQuestions = prev.questions.map((question) => {
+        if (question.id === partId) {
+          const updatedAnswers = question.answers;
+          const existingAnswerIndex = updatedAnswers.findIndex(
+            (answer) => answer.id === item.id
+          );
+          if (existingAnswerIndex !== -1) {
+            updatedAnswers[existingAnswerIndex] = {
+              id: item.id,
+              value: e.target.value.trim(),
+            };
+          } else {
+            updatedAnswers.push({ id: item.id, value: e.target.value.trim() });
+          }
+          return { ...question, answers: updatedAnswers };
+        }
+        return question;
+      });
+      return { ...prev, questions: updatedQuestions };
+    });
+  };
+  console.log(answersArr);
+
   return (
     <div className="bg-[#f8f9fa]">
       <div className="flex justify-center items-center gap-[1rem] p-[2rem]">
@@ -44,13 +100,40 @@ const Reading = () => {
               }}
             />
           </div>
-          <div className="xl:col-span-5 col-span-12 flex flex-col p-[1rem] h-[40rem] overflow-y-auto">
+          <div className="xl:col-span-5 col-span-12 flex flex-col p-[1rem] max-h-[40rem] overflow-y-auto">
             <div className="font-bold my-[1.5rem]">
               <div
                 dangerouslySetInnerHTML={{
                   __html: data?.questions[0].title || '',
                 }}
               />
+            </div>
+            <div className="flex flex-col gap-[1rem] p-[1rem]">
+              {data?.questions[0].answers.map((item, i: number) => {
+                return (
+                  <div className="flex gap-[1rem]">
+                    <label
+                      className="bg-[#e8f2ff] w-[35px] aspect-square flex justify-center items-center rounded-full text-[#35509a] font-bold"
+                      htmlFor={`${index + i}`}
+                    >
+                      {i + 1}
+                    </label>
+                    <input
+                      className="border-[#bdc5d1] border-2 rounded-xl px-[0.5rem]"
+                      key={index}
+                      type="text"
+                      value={
+                        answersArr?.questions
+                          .find((question) => question?.id === partId)
+                          ?.answers.find((answer) => answer?.id === item.id)
+                          ?.value || ''
+                      }
+                      onChange={(e) => handleGetValue(e, item)}
+                      name={`${index + 1}`}
+                    />
+                  </div>
+                );
+              })}
             </div>
           </div>
         </div>
